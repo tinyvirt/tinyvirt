c1ca4d17fab30e36a68a653120fe1e4d1c93557c
fanyang <fanyang89@outlook.com>
2025-11-22 13:05:39 +0800

    Add more files

---
+++ cmd/easyvirt/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/phsym/console-slog"
+	"github.com/urfave/cli/v3"
+)
+
+var rootCmd = &cli.Command{
+	Name: "easyvirt",
+}
+
+func main() {
+	slog.SetDefault(slog.New(console.NewHandler(os.Stderr, &console.HandlerOptions{Level: slog.LevelInfo})))
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer cancel()
+
+	err := rootCmd.Run(ctx, os.Args)
+	if err != nil {
+		slog.Error("unexpected error", "err", err)
+	}
+}
