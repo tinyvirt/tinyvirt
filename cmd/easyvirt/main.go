c7daba5f2ecbbfade4d66d7f14f6772128befe4e
fanyang <fanyang89@outlook.com>
2025-11-22 13:42:07 +0800

    Start gRPC server

--- cmd/easyvirt/main.go
+++ cmd/easyvirt/main.go
@@ -2,17 +2,62 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
+	"net"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/fanyang89/easyvirt/proto"
+	ev "github.com/fanyang89/easyvirt/v1"
 	"github.com/phsym/console-slog"
 	"github.com/urfave/cli/v3"
+	"google.golang.org/grpc"
 )
 
 var rootCmd = &cli.Command{
 	Name: "easyvirt",
+	Commands: []*cli.Command{
+		{
+			Name: "server",
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:    "addr",
+					Aliases: []string{"a"},
+					Value:   ":50051",
+				},
+			},
+			Action: func(ctx context.Context, cmd *cli.Command) error {
+				addr := cmd.String("addr")
+
+				service, err := ev.NewEasyVirtServer()
+				if err != nil {
+					return err
+				}
+
+				listener, err := net.Listen("tcp", addr)
+				if err != nil {
+					return fmt.Errorf("listen failed, %w", err)
+				}
+
+				server := grpc.NewServer()
+				proto.RegisterEasyVirtServer(server, service)
+
+				go func(ctx context.Context) {
+					err := server.Serve(listener)
+					if err != nil {
+						slog.Error("")
+						os.Exit(1)
+					}
+				}(ctx)
+
+				<-ctx.Done()
+				server.GracefulStop()
+				return nil
+			},
+		},
+	},
 }
 
 func main() {
